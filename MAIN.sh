4db956ced726bc23f688594c0a105af840f2f916
Mustafaozver <mustafa.ozver@hotmail.com>
2024-10-13 16:52:24 +0300

    13.10.2024-2

--- MAIN.sh
+++ MAIN.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo "sh ./home/admin/sudo.sh" | sudo su &
+echo "sh /home/admin/sudo.sh" | sudo su &
 
-echo "sh ./home/kiosk/kiosk.sh" | sudo su kiosk &
+echo "sh /home/kiosk/kiosk.sh" | sudo su kiosk &
+
+while true; do
+    sleep 1
+done

8840ff8500575135c02cfe542c868c2e765ff7ca
Mustafaozver <mustafa.ozver@hotmail.com>
2024-10-15 11:10:53 +0300

    14.10.2024-9

--- MAIN.sh
+++ MAIN.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "sh /home/sandbox/sandbox.sh" | sudo su sandbox &
+## echo "sh /home/sandbox/sandbox.sh" | sudo su sandbox &
 
-echo "sh /home/kiosk/kiosk.sh" | sudo su kiosk &
+## echo "sh /home/kiosk/kiosk.sh" | sudo su kiosk &
+
+echo "sh /run/live/medium/kiosk.sh" | sudo su kiosk &
 
 while true; do
     sleep 1
